99bc7fb57e3ce7782f9a02821dff5c701cc3a6d7
Kyo <33697576+kyo1337@users.noreply.github.com>
2020-04-21 19:52:27 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,20 @@
 var request = require("request");
 const readline = require("readline");
 
+// banner
+console.log('|--------------------------------|');
+console.log('| Simple Bot SimSimi Request Api |');
+console.log('|--------------------------------|');
+console.log('| Author  : Kyuoko               |');
+console.log('| Code    : NodeJS               |');
+console.log('| Youtube : Local Heart          |');
+console.log('| Api Trial Limit Request 100    |');
+console.log('|--------------------------------|');
+
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
-	prompt: 'Saya : ',
+	prompt: '|[~] Saya : ',
 	});
 
 function simi() {
@@ -24,7 +34,7 @@ function simi() {
 		},
 		function (error, headers, body){
 			if (error) return console.error(error);
-			console.log('Simi : ', body['atext']);
+			console.log('|[!] Simi :',body['atext']);
 			rl.prompt();
 		});
 	});
